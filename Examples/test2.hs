6e440c3af9879de8c163acc1101ad00019491057
Deian Stefan <deian@cs.stanford.edu>
2010-12-09 14:11:16 -0800

    liftM2 LrefD example

--- Examples/test2.hs
+++ Examples/test2.hs
@@ -10,7 +10,7 @@ hLabel = HSL (Map.singleton (HSC 3) L3) L1 :: HSLabel
 mLabel = HSL (Map.singleton (HSC 3) L2) L2 :: HSLabel
 lLabel = HSL (Map.singleton (HSC 3) L1) L1 :: HSLabel
 
-main = evalHS $ do
+test1 = evalHS $ do
   h <- lref hLabel 32
   l <- lref lLabel 42
   hD <- lrefD hLabel 32
@@ -18,3 +18,11 @@ main = evalHS $ do
   setClearance mLabel
   sequence [labelOfR l, labelOfR h, Just `liftM` labelOfRD lD, Just `liftM` labelOfRD hD]
   
+test2 = evalHS $ do
+ hD <- lrefD hLabel 32
+ lD <- lrefD lLabel 42
+ hrD' <- withClearance mLabel $
+     return $ liftM2 (+) lD hD
+ openRD hrD'
+
+main = test1 >>= print >> test2 >>= print
