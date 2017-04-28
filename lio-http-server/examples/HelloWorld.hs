d9b110b2f873d6793a1ac0d7b481352617733922
Khaled Ahmad <koahmad98@gmail.com>
2017-04-27 19:59:20 -0700

    Changed HelloWorld to take advantage of Responses.hs

--- lio-http-server/examples/HelloWorld.hs
+++ lio-http-server/examples/HelloWorld.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 import LIO.HTTP.Server
+import LIO.HTTP.Server.Responses
 
 main :: IO ()
 main = server 3000 "127.0.0.1" app
 
 app :: DCApplication
-app _ = do
-  return $ Response status200 [] "Hello World!"
+app _ = return (okHtml "Hello World!")
